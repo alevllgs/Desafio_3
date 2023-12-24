a2611060d298c4af3d00658ff5d86c881cc2c3b1
Alejandro Villegas <alejandro.villegas.m@gmail.com>
2023-12-23 21:16:10 -0300

    fin desafio

--- assets/js/4_color.js
+++ assets/js/4_color.js
@@ -1,36 +1,3 @@
-/*
-Construye una página web 4_colores.html con las siguientes características (Total:
-5 puntos)
-●
-Crea 4 divs que tengan alto y ancho de 200px y de colores azul, rojo, verde y
-amarillo (insertar estilos con atributo style). Cada uno de los divs debe tener
-un identificador único. (1 Punto)
-●
-Dentro del script agrega el evento para que al hacer click a cada uno de los
-divs, este cambie de color al color negro. Utiliza addEventListener para
-agregar el evento. (1 Punto)
-_
-2
-Crea una página junto a un script que guarde dentro de una variable global
-un color dependiendo de la letra del teclado presionada. (2 Puntos).
-
-La letra a guardará el color rosado.
-La letra s guardará el color naranjo.
-La letra d guardará el color celeste.
-Para guardar el color revisa el tip al final del enunciado.
-
-Crea un nuevo div con el id “key”, de 200 px de ancho y alto y de
-color blanco y borde negro.
-
-Al presionar las teclas a, s o d, se deberá cambiar el color del div
-“key” a rosado, naranjo o celeste respectivamente.
-●
-Siguiendo con la lógica del punto anterior, al presionar las teclas q, w o e se
-deberá crear un div nuevo de las mismas dimensiones antes mencionadas
-con los colores morado, gris y café respectivamente. (1 Punto)
-Tip: El siguiente script te ayudará a realizar acciones en función de que se
-presione una tecla, además en la guía de estudio encontrarás un ejemplo.
-*/
 
 // Elementos del DOM
 
@@ -38,20 +5,12 @@ presione una tecla, además en la guía de estudio encontrarás un ejemplo.
 //si llamo el elemento desde una clase ocupo . .foto document.querySelector(".foto")
 // lo puedo llamar directamente de la caja section, footer, header document.querySelector("#section")
 
-const seleccion_uno = document.querySelector("#seleccion1")
-const seleccion_dos = document.querySelector("#seleccion2")
-const seleccion_tres = document.querySelector("#seleccion3")
-const verificar = document.querySelector("#boton")
-
-
 // Variables
-
 
 // Funciones
 
-
 // Eventos
-onkeydown
+/* onkeydown
 
 verificar.addEventListener("click", () => {
     const valor1 = seleccion_uno.value
@@ -73,3 +32,69 @@ verificar.addEventListener("click", () => {
 
     }
 })
+*/
+
+
+function cambiarColorNegro(event) {
+    let elemento = event.target;
+    elemento.style.backgroundColor = "black"
+}
+
+
+document.getElementById("div1").addEventListener("click", cambiarColorNegro)
+document.getElementById("div2").addEventListener("click", cambiarColorNegro)
+document.getElementById("div3").addEventListener("click", cambiarColorNegro)
+document.getElementById("div4").addEventListener("click", cambiarColorNegro)
+
+
+
+//PARTE 2
+let keyElement = document.getElementById("key")
+// la funcion se ejecuta el prasionar la tecla, puede hacerse con IF, prefiero el Switch
+function cambiarColor(event) {
+    let tecla = event.key.toLowerCase()
+    switch (tecla) {
+        //cambiar color al presionar tecla
+        case 'a':
+            keyElement.style.backgroundColor = 'pink'
+            break
+        case 's':
+            keyElement.style.backgroundColor = 'orange'
+            break
+        case 'd':
+            keyElement.style.backgroundColor = 'lightblue'
+            break
+
+    }
+}
+
+function NuevoDiv(event) {
+    let tecla = event.key.toLowerCase()
+    switch (tecla) {
+        // Crea un nuevo DIV     
+        case 'q':
+            crearNuevoDiv('purple')
+            break
+        case 'w':
+            crearNuevoDiv('gray')
+            break
+        case 'e':
+            crearNuevoDiv('brown')
+            break
+    }
+}
+
+//esta fx crea el div directo del js
+function crearNuevoDiv(colorFondo) {
+    let nuevoDiv = document.createElement('div')
+    nuevoDiv.style.backgroundColor = colorFondo
+    document.body.appendChild(nuevoDiv)
+    // appendChild añade el div dentro del body
+    // se le añade el colorFondo
+    //igual podria añadir dentro del mismo switch todas las letras solo en uno
+}
+
+document.addEventListener('keydown', cambiarColor)
+document.addEventListener('keydown', NuevoDiv)
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,18 @@
-/*
-Se tiene como base el siguiente ejercicio que cambia el color de fondo de un elemento de
-HTML al hacerle click (3 Puntos)
 
-2.1.
-El código debe estar en 2 archivos: pintar.html y script.js. El script debe
-estar dentro de la carpeta assets/js (1 Punto).
-Para obtener el puntaje debes entregar los archivos cumplimiendo la
-estructura pedida en conjunto con los requerimientos de 3.2 y 3.3
-2.2.
-Modifica la función para que reciba el elemento clickeado de forma de no
-tener que seleccionarlo nuevamente dentro de la función (1 Punto).
-Para obtener el puntaje, debes entregar los archivos pintar.html y script.js
-funcionando en conjunto con el código modificado.
-Tip: revisa en la “Guía - Funciones” las funciones anónimas.
-2.3.
-Modifica el código anterior para poder pasarle un color como argumento a la
-función pintar. El color debe ser verde (green) por defecto, al hacer clic en el
-párrafo se debe pasar amarillo como color. (1 Punto).
-*/
+let ele1Element = document.getElementById("ele1")
+
+//para que comience con verde
+ele1Element.style.backgroundColor = "green"
+// al click
+ele1Element.addEventListener("click", pintar)
+
+//si es verde dale amarillo, si es amarillo dale verde
+function pintar() {
+    let colorActual = ele1Element.style.backgroundColor
+
+    if (colorActual === "green") {
+        ele1Element.style.backgroundColor = "yellow"
+    } else {
+        ele1Element.style.backgroundColor = "green"
+    }
+}
